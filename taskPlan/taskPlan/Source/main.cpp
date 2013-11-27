0ab435d9b0fc9addf48634821ffe9278d96a6589
387358 <t387358@hotmail.com>
2013-11-27 08:07:59 +0800

    version 1.00
    This project already can run in multi-thread, but it's seem to have some
    bugs in insert task when running task queue.

---
+++ taskPlan/taskPlan/Source/main.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+
+#include <QtGui/QApplication>
+
+#include "TaskPriorityQueue.h"
+#include "taskServiceHeader.h"
+
+
+int main(int argc, char *argv[])
+{
+	using namespace std;
+	using namespace taskPlanerNamespace;
+
+	TaskPriorityQueue tq;
+	//tq.queueInsert(10, "Welcome", false);
+	//tq.queueInsert(10, "Sign", false);
+	tq.executeTaskQueue();
+
+	int input;
+	while(1)
+	{
+		cin >> input;
+		tq.queueInsert(10, "Welcome", false);
+	}
+
+	while(1);
+	return 0;
+}
+
